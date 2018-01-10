174fa6b0d303253c19bd3aef9246d3a53160ba2f
Daniel Micay <danielmicay@gmail.com>
2018-01-10 01:40:19 -0500

    switch to in-tree kernel builds

---
+++ AndroidBoard.mk
@@ -0,0 +1,11 @@
+KERNEL_DEFCONFIG := wahoo_defconfig
+KERNEL_DIR := kernel/google/wahoo
+TARGET_KERNEL_APPEND_DTB := true
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+
+include $(TOP)/$(KERNEL_DIR)/AndroidKernel.mk
+
+.PHONY: $(PRODUCT_OUT)/kernel
+$(PRODUCT_OUT)/kernel: $(TARGET_PREBUILT_KERNEL)
+	cp $(TARGET_PREBUILT_KERNEL) $(PRODUCT_OUT)/kernel
