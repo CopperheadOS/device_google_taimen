6d4ef1c9c4cf2be24cc01236b66a36074ce7217d
Daniel Micay <danielmicay@gmail.com>
2018-01-14 09:57:50 -0500

    split debug and production kernel configuration

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,4 +1,8 @@
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+KERNEL_DEFCONFIG := taimen-debug_defconfig
+else
 KERNEL_DEFCONFIG := taimen_defconfig
+endif
 KERNEL_DIR := kernel/google/wahoo
 TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_ARCH := arm64
